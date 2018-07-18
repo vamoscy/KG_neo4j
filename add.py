139a2cf76bfd96649e0e102faff233a33356d3a0
vamoscy <32258269+vamoscy@users.noreply.github.com>
2018-07-18 14:46:01 +0800

    Update add.py

--- add.py
+++ add.py
@@ -1,61 +1,3 @@
-#连接服务器并远程操作
-'''
-import json
-import paramiko
-
-
-def connect(host):
-    'this is use the paramiko connect the host,return conn'
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try:
-        #        ssh.connect(host,username='root',allow_agent=True,look_for_keys=True)
-        ssh.connect(host, username='', password='', allow_agent=True)
-        return ssh
-    except:
-        return None
-
-
-def command(args, outpath):
-    'this is get the command the args to return the command'
-    cmd = '%s %s' % (outpath, args)
-    return cmd
-
-
-def exec_commands(conn, cmd):
-    'this is use the conn to excute the cmd and return the results of excute the command'
-    stdin, stdout, stderr = conn.exec_command(cmd)
-    results = stdout.read()
-    return results
-
-
-def excutor(host, outpath, args):
-    conn = connect(host)
-    if not conn:
-        return [host, None]
-    # exec_commands(conn,'chmod +x %s' % outpath)
-    cmd = command(args, outpath)
-    result = exec_commands(conn, cmd)
-    result = json.dumps(result)
-    return [host, result]
-
-
-def copy_module(conn, inpath, outpath):
-    'this is copy the module to the remote server'
-    ftp = conn.open_sftp()
-    ftp.put(inpath, outpath)
-    ftp.close()
-    return outpath
-
-
-if __name__ == '__main__':
-    print
-    json.dumps(excutor('192.168.1.165', 'ls', ' -l'), indent=4, sort_keys=True)
-    print
-    copy_module(connect('192.168.1.165'), 'kel.txt', '/root/kel.1.txt')
-    exec_commands(connect('192.168.1.165'), 'chmod +x %s' % '/root/kel.1.txt')
-'''
-
 #连接数据库
 from neo4j.v1 import GraphDatabase
 driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "1234"))
@@ -65,7 +7,6 @@ import os,shutil
 from zhon.hanzi import punctuation
 import re
 from xpinyin import Pinyin
-from PIL import Image
 
 
 def get_name(tx):
@@ -86,55 +27,9 @@ def get_id(tx,name):
         return id_list
 
 def create_image_url(tx,image_url):
-    tx.run('create (:pic{name: '  ', image_url: $image_url}) ', image_url=image_url)
+    tx.run('create (:logo{name: '  ', image_url: $image_url}) ', image_url=image_url)
     return None
 
-def resize(file, ID, name, new_root, mwidth=40, mheight=40):
-    p=Pinyin()
-    image_path='/home/ftpuser/www/images/logos/' + str(ID) + '-' + p.get_pinyin(name) + '/' + file
-    new_path=new_root + file
-    with Image.open(image_path) as img:
-        w, h = img.size
-        if w <= mwidth and h <= mheight:
-            print(file, 'is OK.')
-        if (1.0 * w / mwidth) > (1.0 * h / mheight):
-            if file.endswith('jpg') | file.endswith('jpeg') | file.endswith('JPG'):
-                try:
-                    scale = 1.0 * w / mwidth
-                    resized = img.resize((int(w / scale), int(h / scale)), Image.ANTIALIAS)
-                    resized.save(new_path, format="jpeg")
-                    print(file, ' successful')
-                except:
-                    print(image_path,'cannot be resized')
-            elif file.endswith('png') | file.endswith('PNG'):
-                try:
-                    scale = 1.0 * w / mwidth
-                    resized = img.resize((int(w / scale), int(h / scale)), Image.ANTIALIAS)
-                    resized.save(new_path, format="png")
-                    print(file, ' successful')
-                except:
-                    print(file,'cannot be resized')
-            else:
-                    print(file, 'cannot be resized')
-        else:
-            if file.endswith('jpg') | file.endswith('jpeg') | file.endswith('JPG'):
-                try:
-                    scale = 1.0 * h / mheight
-                    resized = img.resize((int(w / scale), int(h / scale)), Image.ANTIALIAS)
-                    resized.save(new_path, format="jpeg")
-                    print(file, ' successful')
-                except:
-                    print(file,'cannot be resized')
-            elif file.endswith('png') | file.endswith('PNG'):
-                try:
-                    scale = 1.0 * h / mheight
-                    resized = img.resize((int(w / scale), int(h / scale)), Image.ANTIALIAS)
-                    resized.save(new_path, format="png")
-                    print(file, ' successful')
-                except:
-                    print(file,'cannot be resized')
-            else:
-                print(file, 'cannot be resized')
 
 def rename(file,new_root):
     print(new_root)
@@ -146,7 +41,7 @@ def rename(file,new_root):
     return new_name
 
 def create_rel(tx,image_url,ID):
-    tx.run('match (n1:pic), (n2:brand) '
+    tx.run('match (n1:logo), (n2:brand) '
            'where n1.image_url= $image_url and ID(n2)= toInt($ID) '
            'create (n2)-[r:logo_is]->(n1) ', image_url=image_url, ID=ID)
     return None
@@ -168,32 +63,49 @@ def create_node_rel(root,name):
     os.mkdir(new_root)
     for file in os.listdir(root):
         shutil.copy(os.path.join(root,file), os.path.join(new_root, file))
-        resize(file, ID, name, new_root)
         new_name = rename(file, new_root)
         image_url = 'http://192.168.11.172:8780//images/logos/' + str(ID) + '-' + p.get_pinyin(name) + '/' + new_name
         driver.session().write_transaction(create_image_url, image_url)
         driver.session().write_transaction(create_rel, image_url, ID)
 
+
 def add_logos(rootdir):
     p=Pinyin()
     name_list=driver.session().read_transaction(get_name)
     list_dirs = os.walk(rootdir)
+    existed= []
+    for root, dirs, files in os.walk('/home/ftpuser/www/images/logos/'):
+        if len(os.listdir(root)) != 0:
+            existed.append(root.split('/')[-1])
     for root, dirs, files in list_dirs:
         name = root.split('/')[-1].strip()
         if name != '':
             name= re.sub("[{}]+".format(punctuation), "", name)
             if name in name_list:
-                print(name, 'existed')
-                for file in os.listdir(root):
-                    in_put=input(file+' add? y/n')
-                    if in_put=='y' :
-                        driver.session().write_transaction(create_brand_name, name)
-                        ID = driver.session().read_transaction(get_id, name)
-                        new_root = '/home/ftpuser/www/images/logos/' + str(ID) + '-' + p.get_pinyin(name) + '/'
-                        shutil.copy(os.path.join(root,file), os.path.join(new_root, file))
-                        resize(file, ID, name, new_root)
+                ID = driver.session().read_transaction(get_id, name)
+                full_name= str(ID) + '-' + p.get_pinyin(name)
+                if full_name in existed:
+                    print(name, 'existed')
+                    for file in os.listdir(root):
+                        in_put=input(file+' add? y/n')
+                        if in_put=='y' :
+                            driver.session().write_transaction(create_brand_name, name)
+                            ID = driver.session().read_transaction(get_id, name)
+                            new_root = '/home/ftpuser/www/images/logos/' + str(ID) + '-' + p.get_pinyin(name) + '/'
+                            shutil.copy(os.path.join(root,file), os.path.join(new_root, file))
+                            new_name = rename(file, new_root)
+                            image_url = 'http://192.168.11.172:8780//images/logos/' + str(ID) + '-' + p.get_pinyin(name) + '/' + new_name
+                            driver.session().write_transaction(create_image_url, image_url)
+                            driver.session().write_transaction(create_rel, image_url, ID)
+                else:
+                    ID = driver.session().read_transaction(get_id, name)
+                    new_root='/home/ftpuser/www/images/logos/' + str(ID) + '-' + p.get_pinyin(name) + '/'
+                    os.mkdir(new_root)
+                    for file in os.listdir(root):
+                        shutil.copy(os.path.join(root, file), os.path.join(new_root, file))
                         new_name = rename(file, new_root)
-                        image_url = 'http://192.168.11.172:8780//images/logos/' + str(ID) + '-' + p.get_pinyin(name) + '/' + new_name
+                        image_url = 'http://192.168.11.172:8780//images/logos/' + str(ID) + '-' + p.get_pinyin(
+                            name) + '/' + new_name
                         driver.session().write_transaction(create_image_url, image_url)
                         driver.session().write_transaction(create_rel, image_url, ID)
             else:
