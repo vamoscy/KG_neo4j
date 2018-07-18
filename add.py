1e73d1c341a64e75402e60daa16b0ba0e7652565
vamoscy <32258269+vamoscy@users.noreply.github.com>
2018-07-18 14:16:11 +0800

    Update add.py

--- add.py
+++ add.py
@@ -1,61 +1,3 @@
-#连接服务器并远程操作
-'''
-import json
-import paramiko
-
-
-def connect(host):
-    'this is use the paramiko connect the host,return conn'
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try:
-        #        ssh.connect(host,username='root',allow_agent=True,look_for_keys=True)
-        ssh.connect(host, username='', password='', allow_agent=True)
-        return ssh
-    except:
-        return None
-
-
-def command(args, outpath):
-    'this is get the command the args to return the command'
-    cmd = '%s %s' % (outpath, args)
-    return cmd
-
-
-def exec_commands(conn, cmd):
-    'this is use the conn to excute the cmd and return the results of excute the command'
-    stdin, stdout, stderr = conn.exec_command(cmd)
-    results = stdout.read()
-    return results
-
-
-def excutor(host, outpath, args):
-    conn = connect(host)
-    if not conn:
-        return [host, None]
-    # exec_commands(conn,'chmod +x %s' % outpath)
-    cmd = command(args, outpath)
-    result = exec_commands(conn, cmd)
-    result = json.dumps(result)
-    return [host, result]
-
-
-def copy_module(conn, inpath, outpath):
-    'this is copy the module to the remote server'
-    ftp = conn.open_sftp()
-    ftp.put(inpath, outpath)
-    ftp.close()
-    return outpath
-
-
-if __name__ == '__main__':
-    print
-    json.dumps(excutor('192.168.1.165', 'ls', ' -l'), indent=4, sort_keys=True)
-    print
-    copy_module(connect('192.168.1.165'), 'kel.txt', '/root/kel.1.txt')
-    exec_commands(connect('192.168.1.165'), 'chmod +x %s' % '/root/kel.1.txt')
-'''
-
 #连接数据库
 from neo4j.v1 import GraphDatabase
 driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "1234"))
@@ -86,7 +28,7 @@ def get_id(tx,name):
         return id_list
 
 def create_image_url(tx,image_url):
-    tx.run('create (:pic{name: '  ', image_url: $image_url}) ', image_url=image_url)
+    tx.run('create (:logo{name: '  ', image_url: $image_url}) ', image_url=image_url)
     return None
 
 def resize(file, ID, name, new_root, mwidth=40, mheight=40):
@@ -146,7 +88,7 @@ def rename(file,new_root):
     return new_name
 
 def create_rel(tx,image_url,ID):
-    tx.run('match (n1:pic), (n2:brand) '
+    tx.run('match (n1:logo), (n2:brand) '
            'where n1.image_url= $image_url and ID(n2)= toInt($ID) '
            'create (n2)-[r:logo_is]->(n1) ', image_url=image_url, ID=ID)
     return None
